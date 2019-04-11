37bd1e9bae014a32f3b9d5435dfaafbd90f4e042
Gonzalo <gonzaponte@gmail.com>
2019-04-11 13:55:01 +0300

    Add missing argument in DeviceClient.__setitem__

--- devserve/clients.py
+++ devserve/clients.py
@@ -51,7 +51,7 @@ class DeviceClient:
         raise AttributeError('Attribute {} is not available'.format( item))
 
     def __setitem__(self, key, value):
-        setattr(self, key)
+        setattr(self, key, value)
 
     def __getitem__(self, key):
         return getattr(self, key)
